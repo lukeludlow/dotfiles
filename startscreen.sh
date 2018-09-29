6fcbd88a28247b6ccfe2ed10dbb88bae09616ba7
lukeludlow <lukeludlow@live.com>
2018-09-29 12:53:09 -0600

    start screen with wal theme

--- startscreen.sh
+++ startscreen.sh
@@ -1,3 +1,4 @@
 xrandr --newmode "1280x800" 173.00 1920 2048 2248 2576 1080 1083 1088 1120 -hsync +vsync
 xrandr --addmode Virtual1 1280x800
 xrandr --output Virtual1 --mode 1280x800
+wal -i ~/Pictures/girl_ocean.jpg
